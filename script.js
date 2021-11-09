4c1504d039f56c9dd587b49d919232bec1ffda3a
RobbieMarseglia <robbie@marseglia.co.uk>
2021-11-09 10:22:56 +0000

    highlighting states

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// document.getElementById("flat-canvas").addEventListener("click", drawCircle);
-// let RADIUS = 40;
-// const nodes = [];
-// var id = 0;
-
-// function drawCircle(event) {
-//     var c = document.getElementById("flat-canvas");
-//     var ctx = c.getContext("2d");
-
-//     var x = event.clientX - c.offsetLeft;
-//     var y = event.clientY - c.offsetTop;
-
-//     var canv_width = ctx.canvas.clientWidth;
-//     var canv_height = ctx.canvas.clientHeight;
-
-//     // Only draw circle if completely within canvas
-//     if ((y+RADIUS < canv_height-5) && (y-RADIUS > 5) && (x+RADIUS < canv_width-5) && (x-RADIUS > 5)){
-//         const n = new Node(id, x, y);
-//         n.draw(ctx);
-//         nodes.push(n);
-//         id++;
-//     }
-// }
-
 let RADIUS = 40;
 const nodes = [];
 var id = 0;
@@ -42,15 +18,10 @@ class Node {
         ctx.beginPath();
         ctx.arc(this.x, this.y, RADIUS, 0, 2 * Math.PI);
         ctx.fill();
-        console.log(highId);
         if (this.id == highId) {
             ctx.strokeStyle = "#ff0000";
-            ctx.stroke();
-        } else {
-            console.log("black");
-            ctx.strokeStyle = "#ffffff";
-            ctx.stroke();
         }
+        ctx.stroke();
         ctx.restore();
     }
 
@@ -89,15 +60,6 @@ var MouseEvent = function(canvas, callback) {
 
 }
 
-// function isNodeClicked(node, x, y) {
-//     var dx = node.x - x;
-//     var dy = node.y - y;
-//     if (dx*dx + dy*dy < RADIUS*RADIUS) {
-//         return true;
-//     }
-//     return false;
-// }
-
 function nodeUnderMouse(x, y) {
     for (let i=0; i< nodes.length; i++) {
         var node = nodes[i];
@@ -116,11 +78,6 @@ ctx.fillStyle = "#fcfcfc";
 var fromX = 0;
 var fromY = 0;
 
-var s = new Node(id, 100, 100);
-nodes.push(s);
-s.draw(ctx);
-id++
-
 var state = null;
 
 var me = new MouseEvent(canvas,
@@ -136,26 +93,15 @@ var me = new MouseEvent(canvas,
                 if (stateId != -1) {
                     state = nodes[stateId];
                     state.dragging = true;
+                    highId = state.id;
                     canvas.style.cursor = "move";
                 } else {
                     var s = new Node(id, x, y);
                     nodes.push(s);
                     highId = id;
                     id++;
+                    state = s;
                 }
-
-                console.log("down");
-                // } else {
-                //     var s = new Node(id, x, y);
-                //     nodes.push(s);
-                //     highId = id;
-                //     s.draw(ctx);
-                //     id++;
-                // }
-
-                // if (isNodeClicked(state, x, y)) {
-                //     state.dragging = true;
-                // }
                 break;
 
             case 'up':
@@ -188,12 +134,10 @@ var me = new MouseEvent(canvas,
                 }
         }
 
-        if (nodes.length != 0) {
-            if (state && state.dragging) {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                for (let i=0; i<nodes.length; i++) {
-                    nodes[i].draw(ctx);
-                }
+        if (state && (state.dragging || eventType == "down")) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            for (let i=0; i<nodes.length; i++) {
+                nodes[i].draw(ctx);
             }
         }
     }
